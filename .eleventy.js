16b648bc044b2bad3c2609b5a93e113147280bf1
Zhenghong Lieu <lieu@pop-os.localdomain>
2020-10-10 21:22:46 +0800

    Add favicon

--- .eleventy.js
+++ .eleventy.js
@@ -67,6 +67,7 @@ module.exports = function (eleventyConfig) {
 
   eleventyConfig.addPassthroughCopy("img");
   eleventyConfig.addPassthroughCopy("css");
+  eleventyConfig.addPassthroughCopy("favicon.ico");
 
   return {
     dir: {
